a36eeddeadd16e01f3f2eee37c324742d1b5bb2f
plopezgrieco <plopezgrieco@gmail.com>
2025-01-11 13:41:42 +0100

    view: put

--- w04_webspring/src/main/java/es/kolbit/w04_webspring/view/PersonaRestController.java
+++ w04_webspring/src/main/java/es/kolbit/w04_webspring/view/PersonaRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class PersonaRestController {
 	@Autowired
 	private Negocio neg;
 
+	@PutMapping
 	public void add(PersonaDto persona) {
 		neg.add(PersonaMapper.getPersona(persona));
 	}
